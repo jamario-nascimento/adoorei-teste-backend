1a2c3f559e016ed4f7e20b03888c209015a73e2b
Jamário Nascimento <jamariobatista@gmail.com>
2024-01-30 00:08:15 -0300

    Migration and seeder Product

--- database/seeds/ProdutoSeeder.php
+++ database/seeds/ProdutoSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Modules\Produto\Entities\Produto;
-
+use Illuminate\Support\Facades\DB;
 class ProdutoSeeder extends Seeder
 {
     /**
@@ -12,6 +12,24 @@ class ProdutoSeeder extends Seeder
      */
     public function run()
     {
-        factory(Produto::class,5)->create();
+            DB::table('produtos')->insert([
+           'name'  => 'Celular 1',
+           'price'  => 1.800,
+           'description'    => 'Lorem ipsum',
+            ]);
+
+            DB::table('produtos')->insert([
+                'name'  => 'Celular 2',
+                'price'  => 3.200,
+                'description'    => 'Lorem ipsum dolor',
+                 ]);
+
+                 DB::table('produtos')->insert([
+                    'name'  => 'Celular 3',
+                    'price'  => 9.800,
+                    'description'    => 'Lorem ipsum dolor sit amet',
+                     ]);
+
+
     }
 }
